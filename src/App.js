c578290343e5a637875e9d30fdc553304e16dfa2
simbankenya <pharesmuruthi@gmail.com>
2022-02-06 15:37:04 +0300

    updating design of the project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import Radial from './components/Radial';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='underline text-3xl text-red-800'>Radial Chart</h1>
-      <Radial/>
+    <div className="antialiased bg-gray-600 min-h-screen flex justify-center items-center">
+      <div className='flex px-8 py-4'>
+        <div>
+            <h1 className='flex-none text-3xl text-white font-bold uppercase text-center tracking-widest space-y-4'>Trillion-Dollar Club</h1>          
+            <h1 className='text-white text-xl'>Members of Trillion Dollar Club</h1>
+            <p className='tracking-6 text-lg text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
+                Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        
+        <div className='flex justify-center items-center'>
+        <Radial/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Radial.js
+++ src/components/Radial.js
@@ -3,30 +3,50 @@ import {scaleLinear, max, arc, interpolate, scaleOrdinal, schemeCategory10} from
 
 
 function Radial() {
-    const width= 960,
+    const width= 650,
         height = 600,
         chartRadius = height/2 - 40;
     const color = scaleOrdinal(schemeCategory10)
 
     const data = [
-        {name: "Jan", value :432},
-        {name: "Feb", value :340},
-        {name: "Mar", value :389},
-        {name: "Apr", value :432},
-        {name: "Jun", value :379},
-        {name: "Jul", value :460},
-    ]
+        {
+          name: "Apple",
+          value : 2.861,
+        },
+        {
+          name: "Mircrosoft",
+          value: 2.861,
+        },
+        {
+          name : "Saudi Aramco",
+          value : 1.965,
+        },
+        {
+          name : "Google",
+          value : 1.961,
+        },
+        {
+          name : "Apple",
+          value : 1.965,
+        },
+        {
+          name : "Amazon",
+          value: 1.598,
+        },
+        
+      ]
+    
 
     // const chartArcs = useRef(chartArcs)
     const PI = Math.PI,
         arcMinRadius = 10,
         arcPadding = 10,
-        labelPadding = -25,
+        labelPadding = -120,
         numTicks = 10;
 
     //scale
     const scale = scaleLinear()
-     .domain([0, max(data, d => d.value) * 1.1])
+     .domain([0, max(data, d => d.value) * 1.2])
      .range([0, 2 * PI])
 
     const ticks = scale.ticks(numTicks).slice(0,-1)
@@ -46,14 +66,11 @@ function Radial() {
     const numArcs = keys.length
     const arcWidth = (chartRadius - arcMinRadius - numArcs * arcPadding) / numArcs;
 
-    // const radialAxis =
 
-    // const axialAxis =
-
-    // function arcTween(d, i){
-    //     const interpolate = interpolate(0, d.value)
-    //     return t => arcGenerator(interpolate(i), i)
-    // }
+    function arcTween(d, i){
+        const interpolate = interpolate(0, d.value)
+        return t => arcGenerator(interpolate(i), i)
+    }
 
     const rad2deg = angle => angle * 180 / PI;
 
@@ -66,19 +83,19 @@ function Radial() {
                     <g className='r axis'>
                         {/* <circle r={getOuterRadius(i) + arcPadding}/> */}
                         {/* <circle r={25* i + arcPadding}/> */}
-                        <text x={labelPadding} y={-getOuterRadius(i) + arcPadding}>{d.name}</text>
+                        <text className='font-bold text-lg' x={labelPadding} y={-getOuterRadius(i) + arcPadding}>{d.name}</text>
                     </g>
                 ))}
 
                 {ticks.map((d,i) => (
                     <g className='a axis' transform={`rotate(${rad2deg(scale(d)) - 90})`}>
                         {/* <line x2={chartRadius}/> */}
-                        <text style={{textAnchor: (scale(d) >= PI && scale(d) < 2 * PI ? 'end': null)}}x={chartRadius + 10} transform={`rotate(${90 - rad2deg(scale(d))},${chartRadius+ 10},0)`}>{d}</text>
+                        <text className='font-bold text-gray-500' style={{textAnchor: (scale(d) >= PI && scale(d) < 2 * PI ? 'end': null)}}x={chartRadius + 10} transform={`rotate(${90 - rad2deg(scale(d))},${chartRadius+ 10},0)`}>${d}</text>
                     </g>
                 ))}
                 {data.map((d,i) => (
                     <g className='data'>
-                        {console.log(arcGenerator(d))}
+                        {/* {console.log(arcGenerator(d))} */}
                         <path d={arcGenerator({innerRadius: getInnerRadius(i), outerRadius: getOuterRadius(i), startAngle: 0, endAngle: scale(d.value) })} style={{fill: color(d)}} className='arc'/>
                         {/* {console.log(arcGenerator(d))} */}
                         {/* {console.log(arcGenerator({innerRadius: getInnerRadius(i), outerRadius: getOuterRadius(i), endAngle: scale(d) }))} */}
